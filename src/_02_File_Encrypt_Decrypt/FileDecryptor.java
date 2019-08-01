48b4917811a3593c296b1a7bb85e40fb742a0ec8
League Student <league@iMac-9.attlocal.net>
2019-07-31 19:59:25 -0700

    word on part2

--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,6 +1,31 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javax.swing.JOptionPane;
+
 public class FileDecryptor {
 	// Create a program that opens the file created by FileEncryptor and display
 	// the decrypted message to the user in a JOptionPane.
+	public static void main(String[] args) {
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader("src/_02_File_Encrypt_Decrypt/msg.txt"));
+			String s = "";
+			while(br.readLine() != null)
+				s += br.readLine();
+			char [] chars = s.toCharArray();
+			for(int i = 0; i < s.length(); i++)
+			{
+				chars[i]--;
+			}
+			JOptionPane.showMessageDialog(null, chars.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,7 +1,32 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileEncryptor {
 	// Create a program that takes a message from the user.
 	// Use the methods in the String and Character classes to save
 	// an encrypted form of the message to a file
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a string to be encrypted");
+		String str = scanner.next();
+		char[]  chars = new char[str.length()];
+		
+		for(int i = 0; i < str.length(); i++)
+		{
+			char c = str.charAt(i);
+			c++;
+			chars[i] = c;
+		}
+		try {
+			FileWriter fw = new FileWriter("src/_02_File_Encrypt_Decrypt/msg.txt", true);
+			fw.write(chars);
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }

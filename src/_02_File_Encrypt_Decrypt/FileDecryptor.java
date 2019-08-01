2a095b423712912a97fc7747f631ce385810615d
portoaj <andrewmasek@gmail.com>
2019-08-01 01:52:06 -0700

    gettingthere

--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,8 +1,11 @@
 package _02_File_Encrypt_Decrypt;
 
 import java.io.BufferedReader;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 import javax.swing.JOptionPane;
 
@@ -13,16 +16,22 @@ public class FileDecryptor {
 		BufferedReader br;
 		try {
 			br = new BufferedReader(new FileReader("src/_02_File_Encrypt_Decrypt/msg.txt"));
-			String s = "";
-			while(br.readLine() != null)
-				s += br.readLine();
-			char [] chars = s.toCharArray();
-			for(int i = 0; i < s.length(); i++)
+			StringBuilder sb = new StringBuilder("");
+			String s;
+			while((s = br.readLine()) != null)
+				sb.append(s);
+			char [] chars = sb.toString().toCharArray();
+			for(int i = 0; i < chars.length; i++)
 			{
 				chars[i]--;
+				System.out.println(chars[i]);
 			}
-			JOptionPane.showMessageDialog(null, chars.toString());
+			System.out.println(chars.toString());
+			JOptionPane.showMessageDialog(null, Arrays.toString(chars));
 		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
